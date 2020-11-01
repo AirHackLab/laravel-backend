c200cf0c19bd255976bbd41ef47def566b8704af
Duman Haydar <haydar@duman.io>
2020-11-01 14:12:32 +0100

    mosquitto password hash

--- app/Http/Controllers/MosquittoController.php
+++ app/Http/Controllers/MosquittoController.php
@@ -14,14 +14,15 @@ class MosquittoController extends Controller
         foreach($devices as $device) {
             $array[] = $device->serial . ":" . $this->mosquitto_pw($device->password);
         }
-        return response(implode("\n", $array), 200)
-        ->header('Content-Type', 'text/plain');
+        return response(implode("\n", $array), 200)->header('Content-Type', 'text/plain');
     }
 
-    private function mosquitto_pw($plain, $algo = 'sha256', $iterations = 901, $saltlen = 12, $keylen = 24)
+    private function mosquitto_pw($plain)
     {
-        $salt = base64_encode(openssl_random_pseudo_bytes($saltlen));
-        $hash = hash_pbkdf2($algo, $plain, $salt, $iterations, $keylen, true);
-        return sprintf('PBKDF2$%s$%d$%s$%s', $algo, $iterations, $salt, base64_encode($hash));
+        $salt_base64="mfJ0Eq3rIDLKG33r";
+        $salt=base64_decode($salt_base64);
+        $hash=hash("sha512",$plain.$salt, true);
+        $hash_base64=base64_encode($hash);
+        return "$6$".$salt_base64."$".$hash_base64;
     }
 }
