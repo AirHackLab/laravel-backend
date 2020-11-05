c8cad2f32d25ce2a0630a2d7adffaa5ce0caea2c
Duman Haydar <haydar@duman.io>
2020-11-05 16:21:15 +0100

    listname

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -5,7 +5,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">{{ __('Users list') }}</div>
+                    <div class="card-header">{{ __($listname . ' list') }}</div>
 
                     <div class="card-body">
                         <table class="table">
