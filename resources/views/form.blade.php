7257e84d20b128ddbe7503979719fd39111efb0d
Duman Haydar <haydar@duman.io>
2020-11-05 17:01:29 +0100

    test

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -5,8 +5,6 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Users list') }}</div>
-
                 <div class="card-body">
                     <form>
                         @foreach ($form as $line)
@@ -14,7 +12,7 @@
                                 <label for="row{{$line['name']}}">{{$line['name']}}</label>
 
                                 @if ($line['dom'] == 'input')
-                                    <input type="{{$line['type']}}" class="form-control" id="row{{$line['name']}}" name="{{$line['name']}}" value="{{isset($data['id']) && $data['id'] != '' ?? $data[$line['name']]}}">
+                                    <input type="{{$line['type']}}" class="form-control" id="row{{$line['name']}}" name="{{$line['name']}}" value="{{isset($data['id']) && $data['id'] != '' ? $data[$line['name']] : ''}}">
                                 @endif
                             </div>
                         @endforeach
