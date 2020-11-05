68427acfb28ee578e327cbf5dfbfd6dd00f55679
Duman Haydar <haydar@duman.io>
2020-11-05 15:22:00 +0100

    add base Controller api function

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,28 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected $client;
+
+    public function __construct()
+    {
+        $this->client = new \GuzzleHttp\Client([
+            // Base URI is used with relative requests
+            'base_uri' => env('APP_URL') . '/api/',
+            // You can set any number of default request options.
+            'timeout' => 30,
+            'headers' => [
+                'api_token' => auth()->user()->api_token
+            ]
+        ]);
+
+    }
+
+    public function api($method, $url, $body = null)
+    {
+        $options = $body ? ['form_params' => $body] : [];
+        $response = $this->client->request($method, $url, $options);
+        $response = $this->client->send($response);
+        return $response;
+    }
 }
