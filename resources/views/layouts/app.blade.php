96a0eba77dfff56679e11c33ab421877793f0034
Duman Haydar <haydar@duman.io>
2020-11-05 16:22:58 +0100

    add menu link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,12 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('user.list') }}">{{ __('Users') }}</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('device.list') }}">{{ __('Devices') }}</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
