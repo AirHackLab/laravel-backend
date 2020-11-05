d9ca9069c5688bf10e63df82eccf4231f8736ca9
Duman Haydar <haydar@duman.io>
2020-11-05 17:17:12 +0100

    add options disabled hide password content

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -12,7 +12,12 @@
                                 <label for="row{{$line['name']}}">{{$line['name']}}</label>
 
                                 @if ($line['dom'] == 'input')
-                                    <input type="{{$line['type']}}" class="form-control" id="row{{$line['name']}}" name="{{$line['name']}}" {{(isset($line['options']) && isset($line['options']['disabled']) && $line['options']['disabled']) ? 'disabled' : ''}} value="{{(isset($data['id']) && $data['id'] != '') && (isset($line['options']) && isset($line['options']['hide']) && !$line['options']['hide']) ? $data[$line['name']] : ''}}">
+                                    <input
+                                        type="{{$line['type']}}"
+                                        class="form-control"
+                                        id="row{{$line['name']}}"
+                                        name="{{$line['name']}}" {{(isset($line['options']) && isset($line['options']['disabled']) && $line['options']['disabled']) ? 'disabled' : ''}}
+                                        value="{{(isset($data['id']) && $data['id'] != '') && !(isset($line['options']) && isset($line['options']['hide']) && !$line['options']['hide']) ? $data[$line['name']] : ''}}">
                                 @endif
                             </div>
                         @endforeach
